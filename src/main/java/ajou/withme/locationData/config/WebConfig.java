5fcaca97a8c2c4d2a9b5ce9f291f2f5f16db1505
HYOTAEK <hyotaek9812@gmail.com>
2021-08-21 22:39:21 +0900

    handle cors

--- src/main/java/ajou/withme/locationData/config/WebConfig.java
+++ src/main/java/ajou/withme/locationData/config/WebConfig.java
@@ -3,9 +3,17 @@ package ajou.withme.locationData.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+    }
 
     @Bean
     public CommonsRequestLoggingFilter commonsRequestLoggingFilter() {
