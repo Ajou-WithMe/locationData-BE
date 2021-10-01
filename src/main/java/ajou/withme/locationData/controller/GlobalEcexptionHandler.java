1516fc72c4f302ece385e2fab335ea24de475d88
HYOTAEK <hyotaek9812@gmail.com>
2021-10-01 14:42:33 +0900

    update GlobalEcexptionHandler

--- src/main/java/ajou/withme/locationData/controller/GlobalEcexptionHandler.java
+++ src/main/java/ajou/withme/locationData/controller/GlobalEcexptionHandler.java
@@ -1,15 +1,22 @@
 package ajou.withme.locationData.controller;
 
 import ajou.withme.locationData.util.ResFormat;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Arrays;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalEcexptionHandler {
 
     @ExceptionHandler(value = Exception.class)
     public ResFormat handleException(Exception e) {
+
+        log.error("------------------------e : "+ e);
+        log.error("------------------------e.toString() : "+e.toString());
+        log.error("------------------------Arrays.toString(e.getStackTrace()) : "+ Arrays.toString(e.getStackTrace()));
         return new ResFormat(false, 500L, e.getMessage());
     }
 
